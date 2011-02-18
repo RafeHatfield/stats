4e790eac2b6048515ca1aa8f1bb4b0a0531d57d8
Mitch Crowe <mitch.crowe@suite101.com>
2011-02-17 16:55:16 -0800

    Refactor search engine / keyword parsing code and tests.

--- app/models/page_view.rb
+++ app/models/page_view.rb
@@ -15,70 +15,16 @@ class PageView < Ohm::Model
 
   before :save, :save_keywords_and_search_engine
 
-  # Given a natural search engine search url return the search engine.
-  # If we don't match any search engine, return nil.
-  def self.get_search_engine(url)
-    
-    # Match "engine_name" followed by a "." with a "?" after it.
-    # NOTE: Search.com matching must be last because other engines
-    #       use "search." in their domain and we want to catch them first.
-    
-    @engine_regex = {
-      :google => /google\..*\?/,
-      :yahoo => /yahoo\..*\?/,
-      :bing => /bing\..*\?/,
-      :msn => /msn\..*\?/,
-      :aol => /aol\..*\?/,
-      :ask => /ask\..*\?/,
-      :yandex => /yandex\..*\?/,
-      :search => /www\.search\.com.*\?/
-    }
-                 
-    @engine_regex.each do |engine, regex|
-      return engine if regex.match(url)
-    end
-
-    return nil
-        
-  end
-
-  # Given a search url and a valid engine symbol return a keyword string.
-  # If the engine symbol is not valid then return nil.
-  def self.get_keywords(url, search_engine)  
-    
-    # Search url make-up:
-    # => [stuff][query_string]=[keyword_string]&[other_stuff]
-    # Keywords are separated by %20 or +, these are converted to " " when unescaped.
-
-    @engine_query_string = {
-      :google => 'q',
-      :bing => 'q',
-      :yahoo => 'p',
-      :msn => 'q',
-      :aol => 'query',
-      :ask => 'q',
-      :yandex => 'text',
-      :search => 'q'
-    }
-    
-    @engine_query_string.each do |engine, query_string|
-      return CGI.unescape(url.split(query_string+"=")[1].split("&")[0]) if search_engine == engine
-    end
-    
-    return nil
-    
-  end
-  
   protected
   
+  # If the referrer_url is from a supported search engine
+  # determine the search engine and keyword phrase and
+  # save these with the page view.
   def save_keywords_and_search_engine
-    # Determine the keyword phrase and search engine for this
-    # referrer url if the referrer is from a search engine
-
-    search_engine = PageView.get_search_engine(self.referer_url)
+    search_engine = SearchURLParser.get_search_engine(self.referer_url)
   
     if search_engine
-      keyword_phrase = PageView.get_keywords(self.referer_url, search_engine)
+      keyword_phrase = SearchURLParser.get_keywords(self.referer_url, search_engine)
     else
       keyword_phrase = nil
     end
