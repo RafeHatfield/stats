8d04d11c2abb5b282514e3284a02e21e94e86f5b
Mitch Crowe <mitch.crowe@suite101.com>
2011-02-22 14:49:10 -0800

    Use memcached with memcache-client gem instead of a ruby hash for uniqueness. [#9573911]

--- app/models/raw_page_view.rb
+++ app/models/raw_page_view.rb
@@ -13,23 +13,26 @@
 #  visited_at      :datetime
 #
 
+require 'memcache'
+
 class RawPageView < ActiveRecord::Base
 
   validate :view_must_be_unique
   validates_presence_of :page_title, :tracked_page_id, :page_url, :page_title, :writer_id, :referrer_url, :cookie_id, :visited_at
   
-  @@uniqueness_hash = {}
-
+  @@uniqueness_cache = MemCache.new "localhost:11211"
   
   def view_must_be_unique
     
     page_view_uuid = "#{tracked_page_id}#{referrer_url}#{cookie_id}".hash.to_s
-
-    if @@uniqueness_hash[page_view_uuid] && visited_at - @@uniqueness_hash[page_view_uuid] <= 30.minutes
+    
+    previous_visited_at = @@uniqueness_cache.get(page_view_uuid)
+    
+    if previous_visited_at && visited_at - previous_visited_at <= 30.minutes
       errors.add(:base, "This view is not unique.")
     end
     
-    @@uniqueness_hash[page_view_uuid] = visited_at
+    @@uniqueness_cache.set(page_view_uuid, visited_at)
     
   end
   
