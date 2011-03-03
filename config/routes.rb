f3f65fe35360523fb0dcbab01b37f66e5ee42b18
Mitch Crowe <mitch.crowe@suite101.com>
2011-03-02 17:56:13 -0800

    Add a link to the dashboard to download csv. [#10434055]

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Stats::Application.routes.draw do
   
   match '/report/:id/:key' => 'reports#index', :as => :dashboard
   match '/test_report/:id' => 'reports#test_index'
-  match '/report/:id/:key/article_views.csv' => 'reports#article_views_csv'
+  match '/report/:id/:key/article_views.csv' => 'reports#article_views_csv', :as => :article_view_csv
 end
