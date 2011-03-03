199c549851be74c2bf6742902ed5b58f8ca39abf
Jerry Tian <jerry.tian@suite101.com>
2011-03-02 16:56:19 -0800

    Added exception in resque job.

--- app/jobs/raw_page_view_job.rb
+++ app/jobs/raw_page_view_job.rb
@@ -3,17 +3,22 @@ class RawPageViewJob
 
   def self.perform(raw_page_view_data)
     # Create a new page view
-    hash = ActiveSupport::JSON.decode(raw_page_view_data)
-    raw_page_view = RawPageView.create(hash)
+    begin
+      hash = ActiveSupport::JSON.decode(raw_page_view_data)
+      raw_page_view = RawPageView.create(hash)
     
-    article = Article.find_and_update_title_or_create({
-      :suite101_article_id => raw_page_view.tracked_page_id,
-      :title => raw_page_view.page_title,
-      :writer_id => raw_page_view.writer_id,
-      :permalink => raw_page_view.page_url
-    })
+      article = Article.find_and_update_title_or_create({
+        :suite101_article_id => raw_page_view.tracked_page_id,
+        :title => raw_page_view.page_title,
+        :writer_id => raw_page_view.writer_id,
+        :permalink => raw_page_view.page_url
+      })
 
-    article.increment_page_view_on(raw_page_view.visited_at.to_date)
+      article.increment_page_view_on(raw_page_view.visited_at.to_date)
+    rescue => e
+      logger.info("Exception happened #{e}")
+      raise "#{e}"
+    end
     # (can do this: try updating day/article row, if updated, ok. else insert)
         
     # parse the referrer url to get an array of keywords, and a domain
