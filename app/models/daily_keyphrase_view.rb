010f70dc4f5ccc9582f309f3633ae6767239540e
Mitch Crowe <mitch.crowe@suite101.com>
2011-03-03 13:43:57 -0800

    Remove unused models from the redis system.

--- app/models/daily_keyphrase_view.rb
+++ app/models/daily_keyphrase_view.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+# Schema version: 20110303185633
+#
+# Table name: daily_keyphrase_views
+#
+#  id         :integer         not null, primary key
+#  date       :date
+#  article_id :integer
+#  keyphrase  :string(255)
+#  count      :integer
+#  writer_id  :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class DailyKeyphraseView < ActiveRecord::Base
   belongs_to :article
   
