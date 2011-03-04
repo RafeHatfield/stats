71cde965fdc95dc0d2dc841cf8a76d4207cc28e1
Jerry Tian <jerry.tian@suite101.com>
2011-03-04 12:10:46 -0800

    Added resque-multi-job-forks gem.

--- config/initializers/load_resque.rb
+++ config/initializers/load_resque.rb
@@ -10,4 +10,9 @@ Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
 #   # server env support, defaults to RAILS_ENV or RACK_ENV
 #   config.server_environment = "production"
 # end
+
+# Workers sleep for 3 seconds (default is 5 seconds)
 ENV['INTERVAL'] = '3'
+# 
+# Workers work on more jobs before suicide. See death notes at https://github.com/staugaard/resque-multi-job-forks
+ENV['MINUTES_PER_FORK'] = '5'
