091ddc4451e7fb87cfea5b4ef9a1be442b7888e8
Mitch Crowe <mitch.crowe@suite101.com>
2011-03-08 18:25:36 -0800

    Clean up controller code.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,20 @@
 class ReportsController < ApplicationController
   
+
   before_filter :verify_key, :except => [:test_index]
   
-  def index
+  # Ensure that the supplied id and key match our encoding.
+  # Setup the @user object with authentication info.
+  def verify_key
+    @user = {:id => params[:id], :key => params[:key]}
+    if @user[:id] != Base64.decode64(@user[:key])
+      render :file => "/public/404.html", :status => 404
+      return false
+    end
+  end
+  
+  # Show the user's dashboard.
+  def dashboard
     @start_date = get_selected_date(params[:start_date], 7.days.ago.to_date)
     @end_date = get_selected_date(params[:end_date], Date.today)
 
@@ -15,23 +27,22 @@ class ReportsController < ApplicationController
     
     keyphrase_counts = DailyKeyphraseView.total_keyphrase_counts_for_writer_between(@user[:id], @start_date, @end_date)
     @keyphrase_counts = keyphrase_counts.sort_by {|i| i[1]*-1}
-    
   end
   
+  # Generate a key for the user and redirect to their dashbaord
+  def test_dashboard
+    redirect_to dashboard_url(params[:id], Base64.encode64(params[:id]))    
+  end
+  
+  # Render javascript which will update the #total_page_views element with the lifetime page views of this user.
   def update_total_page_views
-    @start_date = 90.days.ago.to_date
-    @end_date = Date.today
-    view_counts = DailyPageView.view_counts_for_writer_between(@user[:id], @start_date, @end_date)
-    @total_view_counts = view_counts.sum
-    
-    render :js => "$(function(){$('#total_page_views').text(#{@total_view_counts});});"
+    total_page_views = DailyPageView.view_counts_for_writer_between(@user[:id], 90.days.ago.to_date, Date.today).sum
+    render :js => "$(function(){$('#total_page_views').text(#{total_page_views});});"
   end
-  
-  def page_view_sparkline
-    @start_date = Date.today.at_beginning_of_week - 11.weeks
-    @end_date = Date.today
 
-    view_counts = DailyPageView.view_counts_for_writer_between(@user[:id], @start_date, @end_date)
+  # Redirect to a google sparkline showing the cumulative percentage of page views over the last 12 weeks.
+  def page_view_sparkline
+    view_counts = DailyPageView.view_counts_for_writer_between(@user[:id], Date.today.at_beginning_of_week - 11.weeks, Date.today)
     
     view_counts_by_week = []
     view_counts.in_groups_of(7, false) do |week_counts|
@@ -42,51 +53,14 @@ class ReportsController < ApplicationController
     sum = 0
     cumulative_view_counts = view_counts_by_week.map {|count| sum += count}
     cumulative_percentages = cumulative_view_counts.map {|count| (100 * count / total_view_counts).to_i}
-    data_string = cumulative_percentages.join(',')
-    redirect_to "http://chart.apis.google.com/chart?cht=lc&chs=50x16&chco=369E00&chd=t:#{data_string}&chf=bg,s,FFFFFF00&chls=2,1,0"
+
+    redirect_to "http://chart.apis.google.com/chart?cht=lc&chs=50x16&chco=369E00&chd=t:#{cumulative_percentages.join(',')}&chf=bg,s,FFFFFF00&chls=2,1,0"
   end
   
-  def weekly_page_view_graph
-    # We subtract 1 day to use Sunday as the beginning of the week. 
-    @this_start_date = Date.today.at_beginning_of_week - 1.day
-    @this_end_date = Date.today.at_end_of_week - 1.day
-    
-    @last_start_date = @this_start_date - 1.week
-    @last_end_date = @this_start_date - 1.day
-
-    @view_counts_this_week = DailyPageView.view_counts_for_writer_between(@user[:id], @this_start_date, @this_end_date)
-    @view_counts_last_week = DailyPageView.view_counts_for_writer_between(@user[:id], @last_start_date, @last_end_date) 
-    
-    render :layout => false   
-  end
-  
-  def monthly_page_view_graph
-    @this_start_date = Date.today.at_beginning_of_month
-    @this_end_date = Date.today
-    
-    @last_start_date = 1.month.ago.at_beginning_of_month.to_date
-    @last_end_date = 1.month.ago.at_end_of_month.to_date
-
-    @view_counts_this_month = DailyPageView.view_counts_for_writer_between(@user[:id], @this_start_date, @this_end_date)
-    @view_counts_last_month = DailyPageView.view_counts_for_writer_between(@user[:id], @last_start_date, @last_end_date)
-    
-    # Nil pad the arrays so that they contain the same number of days
-    while @view_counts_this_month.count < 31
-      @view_counts_this_month << nil
-    end
-    
-    while @view_counts_last_month.count < 31
-      @view_counts_last_month << nil
-    end
-
-    render :layout => false
-  end
-  
+  # Render a chart showing the view counts for the writer in the last 12 weeks,
+  # summed by week.
   def twelve_week_page_view_graph
-    @start_date = Date.today.at_beginning_of_week - 11.weeks
-    @end_date = Date.today
-
-    view_counts = DailyPageView.view_counts_for_writer_between(@user[:id], @start_date, @end_date)
+    view_counts = DailyPageView.view_counts_for_writer_between(@user[:id], Date.today.at_beginning_of_week - 11.weeks, Date.today)
     
     @view_counts_by_week = []
     view_counts.in_groups_of(7, false) do |week_counts|
@@ -96,6 +70,42 @@ class ReportsController < ApplicationController
     render :layout => false       
   end
   
+  # Render a chart showing this week vs last week's page views.
+  def weekly_page_view_graph
+    @this_start_date = Date.today.at_beginning_of_week - 1.day
+    @this_end_date = Date.today.at_end_of_week - 1.day
+    @last_start_date = @this_start_date - 1.week
+    @last_end_date = @this_start_date - 1.day
+    # NOTE: We subtract 1 day to use Sunday as the beginning of the week,
+    # since this is how mysuite does it currently.
+
+    @view_counts_this_week = DailyPageView.view_counts_for_writer_between(@user[:id], @this_start_date, @this_end_date)
+    @view_counts_last_week = DailyPageView.view_counts_for_writer_between(@user[:id], @last_start_date, @last_end_date) 
+    
+    render :layout => false   
+  end
+  
+  # Render a chart showing this month vs last month's page views.
+  def monthly_page_view_graph
+    @this_start_date = Date.today.at_beginning_of_month
+    @this_end_date = Date.today
+    @last_start_date = 1.month.ago.at_beginning_of_month.to_date
+    @last_end_date = 1.month.ago.at_end_of_month.to_date
+
+    @view_counts_this_month = DailyPageView.view_counts_for_writer_between(@user[:id], @this_start_date, @this_end_date)
+    @view_counts_last_month = DailyPageView.view_counts_for_writer_between(@user[:id], @last_start_date, @last_end_date)
+    
+    # Nil pad the arrays so that they contain the same number of days,
+    # so that the axes display properly.
+    while @view_counts_this_month.count < 31
+      @view_counts_this_month << nil
+    end
+    while @view_counts_last_month.count < 31
+      @view_counts_last_month << nil
+    end
+
+    render :layout => false
+  end
   
   def article_views_csv
     
@@ -108,29 +118,16 @@ class ReportsController < ApplicationController
       end
     end
 
-    # send it to the browsah
     send_data csv_string,
               :type => 'text/csv; charset=iso-8859-1; header=present',
               :disposition => "attachment; filename=article_counts.csv"
     
   end
   
-  def test_index
-    # Generate a key for the user and redirect to their dashbaord
-    redirect_to dashboard_url(params[:id], Base64.encode64(params[:id]))    
-  end
-  
-  def verify_key
-    @user = {:id => params[:id], :key => params[:key]}
-    
-    if @user[:id] != Base64.decode64(@user[:key])
-      render :file => "/public/404.html", :status => 404
-      return false
-    end
-  end
-  
+
   private
   
+  # Parse the date from a rails select_date tag.
   def get_selected_date(date_hash, default)
     if date_hash
       Date.civil(date_hash["year"].to_i, date_hash["month"].to_i, date_hash["day"].to_i)
@@ -138,7 +135,5 @@ class ReportsController < ApplicationController
       default
     end
   end
-  
-
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Stats::Application.routes.draw do
   match '/add_page_view' => 'tracking#add_page_view'
+  
   match '/admin' => 'admin#index'
-  match '/report/:id/:key' => 'reports#index', :as => :dashboard
-  match '/test_report/:id' => 'reports#test_index'
+  
+  match '/report/:id/:key' => 'reports#dashboard', :as => :dashboard
+  match '/test/:id' => 'reports#test_dashboard'
+  
   match '/report/:id/:key/article_views.csv' => 'reports#article_views_csv', :as => :article_view_csv
+  
+  # My suite graphs and data.
   match '/report/:id/:key/weekly_page_view_graph/' => 'reports#weekly_page_view_graph'
   match '/report/:id/:key/monthly_page_view_graph/' => 'reports#monthly_page_view_graph'
   match '/report/:id/:key/twelve_week_page_view_graph/' => 'reports#twelve_week_page_view_graph'
