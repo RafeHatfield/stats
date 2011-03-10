3ac8b33be4b0e8c21d75367d0454be971daabcea
Jerry Tian <jerry.tian@suite101.com>
2011-03-10 10:49:46 -0800

    Tracking all page views.

--- app/controllers/tracking_controller.rb
+++ app/controllers/tracking_controller.rb
@@ -12,9 +12,7 @@ class TrackingController < ApplicationController
       :date => params[:date]
     }
     
-    if rand(3) == 1
-      Resque.enqueue(RawPageViewJob, raw_page_view_data.to_json)
-    end
+    Resque.enqueue(RawPageViewJob, raw_page_view_data.to_json)
     
     # Send a 1px image back to the requester.
     send_file 'public/images/page_view.gif'
