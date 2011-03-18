5d329f5588f9b42d97911ebc027b069e0ac5d517
Jerry Tian <jerry.tian@suite101.com>
2011-03-18 10:31:10 -0700

    Added uniqueness constraint for articles.

--- db/migrate/20110228212009_create_articles.rb
+++ db/migrate/20110228212009_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration
   
   def self.up
     create_table :articles do |t|
-      t.integer :suite101_article_id
+      t.integer :suite101_article_id, :unique => true
       t.string :title
       t.integer :writer_id
       t.string :permalink
