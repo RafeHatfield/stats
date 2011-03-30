ba5c8517a6548fec34a8335356aa1bf03441f386
Jerry Tian <jerry.tian@suite101.com>
2011-03-30 16:58:03 -0700

    Refactored keyphrases

--- app/controllers/keyphrases_controller.rb
+++ app/controllers/keyphrases_controller.rb
@@ -2,24 +2,11 @@ class KeyphrasesController < ApplicationController
   before_filter :set_start_and_end_date
   
   def index
-    page = params[:page] || 1
-    per_page = 20
     @article_id = params[:suite101_article_id]
+    page = params[:page] || 1
     
-    @keyphrase_counts = DailyKeyphraseView.keyphrases_with_total_counts_for_article_between(@article_id, @start_date, @end_date)
-    @keyphrase_counts = @keyphrase_counts[((page.to_i - 1) * per_page)...(page.to_i * per_page)]
-    @keyphrase_counts.instance_eval <<-EVAL
-      def current_page
-        #{page || 1}
-      end
-      def num_pages
-        count
-      end
-      def limit_value                                                                               
-        20
-      end
-    EVAL
-    
+    @keyphrase_counts = DailyKeyphraseView.paginated_keyphrases_with_total_counts_for_article_between(@article_id, @start_date, @end_date, page)
+        
     render :partial => 'article_stats/keyphrases', :locals => {:keyphrase_counts => @keyphrase_counts} and return
   end
 end
--- app/models/daily_keyphrase_view.rb
+++ app/models/daily_keyphrase_view.rb
@@ -42,4 +42,25 @@ class DailyKeyphraseView < ActiveRecord::Base
     # Note: With "..." range rails generates a sql where clause with 
     # date >= d1, date < d2 so we add an extra day to include today.
   end
+  
+  def self.per_page
+    20
+  end
+  
+  def self.paginated_keyphrases_with_total_counts_for_article_between(article_id, start_date, end_date, page)
+    keyphrase_counts = DailyKeyphraseView.keyphrases_with_total_counts_for_article_between(article_id, start_date, end_date)
+    keyphrase_counts = keyphrase_counts[((page.to_i - 1) * per_page)...(page.to_i * per_page)]
+    keyphrase_counts.instance_eval <<-EVAL
+      def current_page
+        #{page || 1}
+      end
+      def num_pages
+        count
+      end
+      def limit_value                                                                               
+        20
+      end
+    EVAL
+    keyphrase_counts
+  end
 end
