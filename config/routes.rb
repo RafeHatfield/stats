d3e20f38166eb806ca3e05ea20d6e529b7bd638b
Rafe Hatfield <rafe.hatfield@suite101.com>
2012-03-15 17:23:30 -0700

    removed routes to turn off stats for writers [#26450939]

--- config/routes.rb
+++ config/routes.rb
@@ -15,35 +15,35 @@ Stats::Application.routes.draw do
   match '/admin' => 'admin#index'
   
   # Dashboards
-  match '/report/:key' => 'reports#dashboard', :as => :dashboard
-  match '/article_report/:key/:article_id' => 'reports#article_dashboard', :as => :article_dashboard
-  match '/test/:writer_id' => 'reports#test_dashboard'
-
-  # CSV
-  match '/report/:key/article_views.csv' => 'reports#article_views_csv', :as => :article_view_csv
-  
-  # Page Views
-  match '/article_page_views/:key/:article_id' => 'page_views#for_article', :as => :article_page_views
-  match '/writer_page_views/:key/:writer_id' => 'page_views#for_writer', :as => :writer_page_views
-  
-  # Keyphrases
-  match '/article_keyphrases/:key/:article_id' => 'keyphrases#for_article', :as => :article_keyphrases
-  match '/writer_keyphrases/:key/:writer_id' => 'keyphrases#for_writer', :as => :writer_keyphrases
-  
-  # Domains
-  match '/writer_domains/:key/:writer_id' => 'domains#for_writer', :as => :writer_domains
-  match '/article_domains/:key/:article_id' => 'domains#for_article', :as => :article_domains
-  
-  # Sources
-  match '/writer_sources/:key/:writer_id' => 'sources#for_writer', :as => :writer_sources
-  match '/article_sources/:key/:article_id' => 'sources#for_article', :as => :article_sources
-    
-  # Articles
-  match '/writer_articles/:key/:writer_id' => 'articles#for_writer', :as => :writer_articles
-  
-  # Votes
-  match '/writer_votes/:key/:writer_id' => 'article_votes#for_writer', :as => :writer_votes
-  match '/article_votes/:key/:article_id' => 'article_votes#for_article', :as => :article_votes
+  # match '/report/:key' => 'reports#dashboard', :as => :dashboard
+  # match '/article_report/:key/:article_id' => 'reports#article_dashboard', :as => :article_dashboard
+  # match '/test/:writer_id' => 'reports#test_dashboard'
+  # 
+  # # CSV
+  # match '/report/:key/article_views.csv' => 'reports#article_views_csv', :as => :article_view_csv
+  # 
+  # # Page Views
+  # match '/article_page_views/:key/:article_id' => 'page_views#for_article', :as => :article_page_views
+  # match '/writer_page_views/:key/:writer_id' => 'page_views#for_writer', :as => :writer_page_views
+  # 
+  # # Keyphrases
+  # match '/article_keyphrases/:key/:article_id' => 'keyphrases#for_article', :as => :article_keyphrases
+  # match '/writer_keyphrases/:key/:writer_id' => 'keyphrases#for_writer', :as => :writer_keyphrases
+  # 
+  # # Domains
+  # match '/writer_domains/:key/:writer_id' => 'domains#for_writer', :as => :writer_domains
+  # match '/article_domains/:key/:article_id' => 'domains#for_article', :as => :article_domains
+  # 
+  # # Sources
+  # match '/writer_sources/:key/:writer_id' => 'sources#for_writer', :as => :writer_sources
+  # match '/article_sources/:key/:article_id' => 'sources#for_article', :as => :article_sources
+  #   
+  # # Articles
+  # match '/writer_articles/:key/:writer_id' => 'articles#for_writer', :as => :writer_articles
+  # 
+  # # Votes
+  # match '/writer_votes/:key/:writer_id' => 'article_votes#for_writer', :as => :writer_votes
+  # match '/article_votes/:key/:article_id' => 'article_votes#for_article', :as => :article_votes
   resources :article_votes
 
   # My suite graphs and data.
